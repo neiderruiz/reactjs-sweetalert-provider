db5d81210fe421420990640dec7c01fba159e42a
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-10-29 17:24:21 -0500

    use alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react';
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 import Button from "./components/Button";
 import SubComponent from "./components/SubComponent";
+import { AlertContext } from './context/alert.context';
 import viteLogo from "/vite.svg";
 
 function App() {
 
+  const { alertSuccess, alertError, alertInfo, alertWarning } = useContext(AlertContext)
 
   return (
     <>
@@ -22,21 +25,54 @@ function App() {
         <Button
           style="success"
           label="Success Button"
+          onClick={() => alertSuccess({
+            title: 'mi nuevo alert',
+            text: 'mi description',
+          })}
         />
 
         <Button
           style="warning"
           label="Warning Button"
+          onClick={() => {
+            alertWarning({
+              title: 'mi nuevo alert warning',
+              text: 'mi description warning'
+            })
+          }}
         />
 
         <Button
           style="info"
           label="Info Button"
+          onClick={() => {
+            alertInfo({
+              title: 'mi nuevo alert Info',
+              text: 'mi description info'
+            })
+          }}
         />
 
         <Button
           style="error"
           label="Error Button"
+          onClick={() => {
+            alertError({
+              title: 'mi nuevo alert error',
+              text: 'mi description error',
+              denyButtonText: 'Cancelar',
+              showDenyButton: true,
+              backdrop: false
+            }).then(response => {
+              console.log(response)
+              if (response.isConfirmed) {
+                alertSuccess({
+                  title: 'Usuario Eliminado',
+                  text: 'Alerta success'
+                })
+              }
+            })
+          }}
         />
       </div>
       <SubComponent />
