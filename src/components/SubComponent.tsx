10b4a0fd01dc957f234d609d2dafa025f5bbe0fb
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-10-29 17:23:29 -0500

    implement alert sub component

--- src/components/SubComponent.tsx
+++ src/components/SubComponent.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
+import { AlertContext } from "../context/alert.context";
 import Button from "./Button";
-
 const SubComponent = () => {
+
+    const { alertSuccess, alertError, alertInfo, alertWarning } = useContext(AlertContext)
 
     return (
         <>
@@ -8,19 +11,41 @@ const SubComponent = () => {
                 <Button
                     style="success"
                     label="Success Subcomponent"
+                    onClick={() => alertSuccess({
+                        title: 'mi nuevo alert',
+                        text: 'mi description',
+                    })}
                 />
                 <Button
                     style="warning"
                     label="Warning Subcomponent"
+                    onClick={() => {
+                        alertWarning({
+                            title: 'mi nuevo alert warning',
+                            text: 'mi description warning'
+                        })
+                    }}
                 />
 
                 <Button
                     style="info"
                     label="Info Subcomponent"
+                    onClick={() => {
+                        alertInfo({
+                            title: 'mi nuevo alert Info',
+                            text: 'mi description info'
+                        })
+                    }}
                 />
                 <Button
                     style="error"
                     label="Error Subcomponent"
+                    onClick={() => {
+                        alertError({
+                            title: 'mi nuevo alert error',
+                            text: 'mi description error'
+                        })
+                    }}
                 />
             </div>
         </>
