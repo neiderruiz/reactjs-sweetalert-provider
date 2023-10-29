e5e29f90cd5e53bfba27501d5efd64d54bbe05a0
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-10-29 17:23:40 -0500

    add AlertProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import AlertProvider from "./provider/alert.provider.tsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AlertProvider>
+      <App />
+    </AlertProvider>
   </React.StrictMode>
 );
